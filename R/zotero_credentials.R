b651e390f79c37489829e6d7f62bfe76ce3504e5
Michal Bojanowski <michal2992@gmail.com>
2020-05-22 19:31:37 +0200

    Add Zotero credentials

---
+++ R/zotero_credentials.R
@@ -0,0 +1,23 @@
+#' Acess Zotero username and key from environment variables.
+#'
+#' @description - `zotero_usr()` - return Zotero user ID
+#'
+#' @description - `zotero_key()` - return Zotero key
+#'
+#' @details Best practice is to store both the user ID and the key in
+#'   read-protected `.Renviron` as environment variables. The two functions
+#'   `ZOTERO_USER` and `ZOTERO_KEY` respectively.
+#'
+#' @name zotero_credentials
+#'
+#' @export
+
+zotero_usr <- function() as.numeric(Sys.getenv("ZOTERO_USER"))
+
+
+
+
+#' @rdname zotero_credentials
+#'
+#' @export
+zotero_key <- function() Sys.getenv("ZOTERO_KEY")
