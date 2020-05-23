d6ac8e623e0e8a3580d1f6caf0141143e6cb324f
Michal Bojanowski <michal2992@gmail.com>
2020-05-23 19:30:49 +0200

    Zotero credentials fetched from options or environment

--- R/zotero_credentials.R
+++ R/zotero_credentials.R
@@ -1,18 +1,27 @@
-#' Acess Zotero username and key from environment variables.
+#' Get Zotero username and key
 #'
 #' @description - `zotero_usr()` - return Zotero user ID
 #'
 #' @description - `zotero_key()` - return Zotero key
 #'
-#' @details Best practice is to store both the user ID and the key in
-#'   read-protected `~/.Renviron` as environment variables. The two functions
-#'   look at `ZOTERO_USER` and `ZOTERO_KEY` respectively.
+#' @details These functions look for Zotero user ID and key by looking into the
+#'   following places and returning as soon as a value found:
+#'
+#'   - Options `zotero.user` and `zotero.key` respectively
+#'   - Environment variables `ZOTERO_USER` and `ZOTERO_KEY` respectively.
+#'
+#' Best practice is to store both the user ID and the key in read-protected
+#' `~/.Renviron` as environment variables.
 #'
 #' @name zotero_credentials
 #'
 #' @export
 
-zotero_usr <- function() as.numeric(Sys.getenv("ZOTERO_USER"))
+zotero_usr <- function() {
+  if(!is.null(getOption("zotero.user"))) return(getOption("zotero.user"))
+  if(Sys.getenv("ZOTERO_USER") != "") return(Sys.getenv("ZOTERO_USER"))
+  stop("no Zotero user ID found, see ?zoterro::zotero_credentials")
+}
 
 
 
@@ -20,4 +29,8 @@ zotero_usr <- function() as.numeric(Sys.getenv("ZOTERO_USER"))
 #' @rdname zotero_credentials
 #'
 #' @export
-zotero_key <- function() Sys.getenv("ZOTERO_KEY")
+zotero_key <- function() {
+  if(!is.null(getOption("zotero.key"))) return(getOption("zotero.key"))
+  if(Sys.getenv("ZOTERO_KEY") != "") return(Sys.getenv("ZOTERO_KEY"))
+  stop("no Zotero key found, see ?zoterro::zotero_credentials")
+}
