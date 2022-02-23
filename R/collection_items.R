9d9a2b30ecd31c3b1d1d70fe3ed7c9c889a66ac9
Salim B <git@salim.space>
2022-02-23 23:02:36 +0100

    Allow to define export format in `save_collection()`

--- R/collection_items.R
+++ R/collection_items.R
@@ -12,16 +12,50 @@ collection_items <- function(key, ...) {
   )
 }
 
-#' @rdname collection_items
+#' Export all items in a collection to file
 #'
-#' @description - `save_collection()` - Write BibTeX file `path` with all items in a collection
+#' Writes a file of the chosen `format` to `path` with all items in a collection.
 #'
-#' @param path path to file
+#' @inheritParams collection_items
+#' @param path Path to file.
+#' @param format Bibliographic data format used for export. One of:
+#'   - `"bibtex"`,
+#'   - `"biblatex"`,
+#'   - `"csljson"`,
+#'   - `"mods"`,
+#'   - `"refer"`,
+#'   - `"rdf_bibliontology"`,
+#'   - `"rdf_dc"`,
+#'   - `"rdf_zotero"`,
+#'   - `"ris"`,
+#'   - `"wikipedia"`
+#'
+#'   For details, see the [relevant Zotero
+#'   documentation](https://www.zotero.org/support/dev/web_api/v3/basics#export_formats).
 #'
 #' @export
-save_collection <- function(key, path, ...) {
-  cat(
-    rawToChar(collection_items(key, query = list(format = "bibtex"), ...)),
-    file = path
+save_collection <- function(key, path, format = "bibtex", ...) {
+
+  format <- match.arg(
+    arg = format,
+    choices = c("bibtex",
+                "biblatex",
+                # "bookmarks", # only returns a list of `<pointers>`...
+                # "coins",     # only returns a list of `<pointers>`...
+                "csljson",
+                "mods",
+                "refer",
+                "rdf_bibliontology",
+                "rdf_dc",
+                "rdf_zotero",
+                "ris",
+                # "tei",       # returns internal server error (500)
+                "wikipedia")
   )
+  res <- collection_items(key, query = list(format = format), ...)
+
+  # all formats other than "wikipedia" are returned as raw vctrs
+  if (format != "wikipedia") res <- rawToChar(res)
+
+  cat(res, file = path)
 }
